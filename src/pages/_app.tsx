74eb3d13982a69a26a94842730d3abc268710b69
Cleiton <cleiton8569@gmail.com>
2024-05-11 11:19:24 -0300

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,32 @@ import LogoImg from '../assets/logo.svg'
 
 import Image from "next/image"
 import { Container, Header } from "../styles/pages/app"
+import { CartProvider } from "use-shopping-cart"
 
 
 globalStyles()
+
 
 function App({ Component, pageProps}: AppProps) {
    
 
     return (
         <Container>
+            <CartProvider
+                cartMode="checkout-session"
+                stripe=""
+                currency="USD"
+                loading={<p aria-live="polite">Loading redux-persist...</p>}
+                shouldPersist={true}
+            
+            >
             <Header>
                 <Image src={LogoImg} alt=""/>
                
 
             </Header>
             <Component {...pageProps} />
+            </CartProvider>   
         </Container>
     ) 
 }
